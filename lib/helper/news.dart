e2f64d11a52e78ede4aa5f13932b565e786468f1
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-08-11 03:02:58 +0530

    News API data has fetched

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -1,3 +1,38 @@
+import 'dart:convert';
+
+import '../models/article_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/article_model.dart';
+import '../models/article_model.dart';
+
 class News {
-  List<ArticleMode> news = [];
+  List<ArticleModel> news = [];
+
+  Future<void> getNews() async {
+    String url =
+        "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0f996e2478b54bbb97086eff0af0896b";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            url: element['url'],
+            urlToImage: element['urlToImage'],
+            publishedAt: element['publishedAt'],
+            content: element['context'],
+          );
+
+          news.add(articleModel);
+        }
+      });
+    }
+  }
 }
